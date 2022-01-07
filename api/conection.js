ed05c0bf733369baff845f69217525d7d9f6c486
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-01-07 07:29:54 -0300

    routes conection

--- api/conection.js
+++ api/conection.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 
 const middlewares = require('../middlewares/tokenRegistrer');
-const apiCharacterRouter = require('./api/character');
-const apiGenreRouter = require('./api/genre');
-const apiMovieRouter = require('./api/movieSerie');
-const apiUsersRouter = require('./api/user');
+const apiUsersRouter = require('../routes/userRout');
+const characterRouter = require('../routes/characters');
+const moviesOrSeriesRouter = require('../routes/moviesOrSeries');
+const genderRouter = require('../routes/genders');
 
 
-router.use('/characters', middlewares.checkToken, apiCharacterRouter);
-router.use('/movies', middlewares.checkToken, apiMovieRouter);
+
+router.use('/characters', middlewares.checkToken, characterRouter);
+router.use('/movies', middlewares.checkToken, moviesOrSeriesRouter);
 router.use('/auth', apiUsersRouter);
 
 
